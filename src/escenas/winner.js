ce8e5ce9a71c5d19e1536f4e3aaec3ec8e45096a
FloresEnzo <enzoflores635@gmail.com>
2023-10-03 02:21:40 -0300

    se modifico

--- src/escenas/winner.js
+++ src/escenas/winner.js
@@ -1,7 +1,7 @@
 class winner  extends Phaser.Scene{
    
     constructor(){
-              super(" winner");
+              super("winner");
     }
      
     preload(){
@@ -9,9 +9,9 @@ class winner  extends Phaser.Scene{
        // para tenerlo disponibles para que la paguna luego la pueda renderizar
     
        //this.load.image('menu2', '/public/img/menu2.png');
-       this.load.image('winner', '/public/img/winner.png');
-       this.load.image('win', '/public/img/win.png');
-       this.load.image('volvermenu', '/public/img/volvermenu.png');
+       this.load.image('winner', '../public/img/winner.png');
+       this.load.image('win', '../public/img/win.png');
+       this.load.image('volvermenu', '../public/img/volvermenu.png');
        //this.load.image('boton',  '/public/img/estrella.png');
        //this.load.image('boton2', '/public/img/estrella.png');
        //this.load.image('boton3', '/public/img/estrella.png');
@@ -33,7 +33,7 @@ class winner  extends Phaser.Scene{
       
       
         ///////////////////
-            this.starbutton   = this.add.image(400,800, 'volvermenu').setInteractive();
+            this.starbutton   = this.add.image(400,800,'volvermenu').setInteractive();
             //this.starbutton.setScale(0.5);
             
         
@@ -46,7 +46,9 @@ class winner  extends Phaser.Scene{
         
 
       }
-    //update
+    update(){
+      
+    }
     
   }
    
