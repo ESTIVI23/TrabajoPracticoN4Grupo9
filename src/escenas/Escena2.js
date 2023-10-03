9a2b46396b8cea57e2bb30bf42b916c1b77c92e5
salvavelazquez <augustorvelazquez@gmail.com>
2023-10-03 00:25:21 -0300

    Otra actualizacion

--- src/escenas/Escena2.js
+++ src/escenas/Escena2.js
@@ -5,6 +5,10 @@ class Escena2 extends Phaser.Scene{
       this.platforms = null;
       this.scoreText = "";
       this.score = 0;
+      this.numPlat = 3;
+      // Variables globales encapsuladas en el constructor
+      this.platformSpeed = 50;
+      this.platformGroup = null;
   }
   
   
@@ -15,45 +19,43 @@ class Escena2 extends Phaser.Scene{
       this.load.image('lava', '../public/img/lavaa.png');
       this.load.image('star', '../public/img/star.png');
       this.load.image('bomb', '../public/img/bomb.png');
+      this.load.image('trofeo', '../public/img/trofeo.png');
       this.load.spritesheet('dude', '../public/img/dude.png', {frameWidth:32, frameHeight:48});
   }
 
   create(){
       //cielo y barras
       this.add.image(400,300,'sky2').setScale(1.7).setTint(0x980B54);
-      //this.add.image(400,300,'sky').setTint(0x980B54);
-      this.platforms = this.physics.add.group();
+      
+      // Crear un grupo de plataformas
+      this.platformGroup = this.physics.add.group();
+      
+      this.trofeo = this.physics.add.group();
 
       this.piso = this.physics.add.staticGroup();
 
-      
+      //Evento de actualizacion
+       this.time.addEvent({
+            delay: 5000,
+            callback: this.crearPlataformas,
+            callbackScope: this,
+            repeat: 1
+        });
 
-      
-      this.platforms.create(600, 400, 'ground');
-      this.platforms.create(50, 250, 'ground');
-      this.platforms.create(400, 80, 'ground');
-      this.platforms.create(750, 220, 'ground');
+        let p1 = this.platformGroup.create(50, 250, 'ground');
+        let p2 = this.platformGroup.create(480, 80, 'ground');
+        let p3 = this.platformGroup.create(750, 220, 'ground');
 
       this.piso.create(150, 577, 'lava').setScale(1).refreshBody();
       this.piso.create(400, 577, 'lava').setScale(1).refreshBody();
       this.piso.create(700, 577, 'lava').setScale(1).refreshBody();
 
-      this.platforms.children.iterate(function(platform) {
-          //platform.setTint(0xFF4F00); // Código de color naranja
-      });
-
-
-      // Establecer la gravedad para el jugador
-     // this.physics.world.gravity.y = 300; // Ajusta este valor según tus necesidades
-
-      // Establecer una gravedad más baja para las plataformas
-     // this.platforms.children.iterate(function (platform) {
-      //    platform.body.gravity.y = 20; // Ajusta este valor según tus necesidades
-      //});
+      this.physics.world.setBounds(0, 0, 800, 600);
+      this.physics.add.collider(this.platformGroup, this.platformGroup);
 
       //Estrella y jugador
-      //this.add.image(400, 300, 'star');
       this.player = this.physics.add.sprite(100, 200, 'dude');
+     
       //physics del player
       this.player.setBounce(0.2); //rebote entre 0 o 1
       this.player.setCollideWorldBounds(true); //no atravesar bordes del area de juego
@@ -81,17 +83,14 @@ class Escena2 extends Phaser.Scene{
       });
 
       //Colision de jugador y plataformas
-      this.physics.add.collider(this.player, this.platforms);
+      this.physics.add.collider(this.player, this.platformGroup);
 
       this.cursors = this.input.keyboard.createCursorKeys();
-
-
-
   
       //Se  agregan las estrellas
       this.stars = this.physics.add.group({
           key: 'star',
-          repeat: 2, //cantidad de estrellas
+          repeat: 5, //cantidad de estrellas
           setXY: { x: 12, y: 0, stepX: 110 } //empieza en la posicion x e y, se repite cada 70 de espacios
       });
 
@@ -101,17 +100,20 @@ class Escena2 extends Phaser.Scene{
       });
 
       //habilita las colisiones de las estrellas con las plataformas
-      this.physics.add.collider(this.stars, this.platforms);
+      this.physics.add.collider(this.stars, this.platformGroup);
 
       //Choque entre las estrellas y el jugador
       this.physics.add.overlap(this.player, this.stars, this.collectStar, null , this );
+      
+       //Choque entre el trofeo y el jugador
+       this.physics.add.overlap(this.player, this.trofeo, this.reaccionTrofeo , null , this );
 
       //Para controlar el puntaje
       this.scoreText = this.add.text(16, 16, 'score: 0', {fontSize: '32px', fill: '#00FFFF'});
 
       //Para agregar las bombas
       this.bombs = this.physics.add.group();
-      this.physics.add.collider(this.bombs, this.platforms);
+      this.physics.add.collider(this.bombs, this.platformGroup);
       this.physics.add.collider(this.player, this.bombs, this.hitBomb, null, this);
 
 
@@ -129,11 +131,21 @@ class Escena2 extends Phaser.Scene{
   }
   update(){
 
-      // Dentro de la función update()
-      // Agrega una velocidad hacia abajo a las plataformas para que parezcan caer
-      this.platforms.children.iterate(function (platform) {
-          platform.setVelocityY(50); // Ajusta la velocidad hacia abajo según tus necesidades
-      });
+    
+      // Mueve las plataformas hacia abajo y recrearlas si es necesario
+      if (this.platformGroup.getChildren().length > 0) {
+        this.platformGroup.children.iterate(function(platform) {
+            platform.setVelocityY(this.platformSpeed);
+            
+            if (platform.y > 600) {
+                console.log("Estoy aqui al finnn!!");
+                platform.destroy();
+                let p1 = this.platformGroup.create(0, 700, '');
+                
+            }
+            
+        }, this);
+      }
 
       if (this.cursors.left.isDown) {
           this.player.setVelocityX(-160);
@@ -154,6 +166,25 @@ class Escena2 extends Phaser.Scene{
           this.player.setVelocityY(-300);
       }
 
+  }
+
+  //Creacion de plataformas
+  crearPlataformas(){
+    let p1 = this.platformGroup.create(50, 200, 'ground');
+    let p2 = this.platformGroup.create(480, 40, 'ground');
+    let p3 = this.platformGroup.create(750, 170, 'ground');
+    this.numPlat +=3;
+
+    if(this.numPlat == 9 ){
+        let p1 = this.trofeo.create(400, 0, 'trofeo');
+        this.physics.add.collider(this.trofeo, this.platformGroup); 
+    }
+    
+
+  } 
+
+  reaccionTrofeo(){
+    this.scene.start('winner');
   }
 
   //Colision entre el jugador y las estrellas
@@ -186,24 +217,14 @@ class Escena2 extends Phaser.Scene{
   }
 
   fire(player, piso){
-  this.time.addEvent({
-    delay: 500,
-    callbackScope: this,
-    callback: function(){
-        this.scene.start('GameO')
-    }
-  })
-   }
-  ///////////////////////
- /* fire(player, piso){
-    this.physics.pause();
     player.setTint(0xff0000);
-    player.anims.play('turn');
-    this.score = 0;
-    //gameOver = true;
-    this.scene.start('GameO');
-    //this.scene.start('Escena2');
-}*/
-////////////////////////////////
+    this.time.addEvent({
+        delay: 500,
+        callbackScope: this,
+        callback: function(){
+            this.scene.start('GameO')
+        }
+    })
+   }
 }
 export default Escena2;
